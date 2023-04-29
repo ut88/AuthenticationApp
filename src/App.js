bc74b85a9405408891ca62225c03069916a846a5
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-04-29 20:11:52 +0530

    Addressing security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route} from 'react-router-dom';
+import { useContext } from 'react';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
+  const authCtx=  useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+       {!authCtx.isLoggedIn&& <Route path='/auth'>
           <AuthPage />
-        </Route>
+        </Route>}
         <Route path='/profile'>
-          <UserProfile />
+        {authCtx.isLoggedIn &&<UserProfile />}
+        {! authCtx.isLoggedIn && <Redirect to='/auth' />}  
         </Route>
+        < Route path='*'><Redirect to='/' /></Route>
       </Switch>
     </Layout>
   );
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -10,16 +10,19 @@ const AuthContext=React.createContext({
 
 
 export const AuthContextProvider=(props)=>{
-    const [token,setToken]=useState(null)
+    const intialToken=localStorage.getItem('token');
+    const [token,setToken]=useState(intialToken)
 
     const userIsLoggedIn=!!token;
 
     const loginHandler=(token)=>{
-       setToken(token)
+       setToken(token);
+       localStorage.setItem('token',token);
     }
 
     const logoutHandler=()=>{
         setToken(null)
+        localStorage.removeItem('token');
     }
 
     const contextValue={
